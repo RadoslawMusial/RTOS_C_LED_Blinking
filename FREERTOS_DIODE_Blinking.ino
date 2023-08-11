7b4f22c878396122433216953ccbafe6ef7878d3
RadoslawMusial <onlynaves@gmail.com>
2023-08-11 03:20:37 +0200

    corrected functions

--- FREERTOS_DIODE_Blinking.ino
+++ FREERTOS_DIODE_Blinking.ino
@@ -1,78 +1,60 @@
-#include <Arduino_FreeRTOS.h> 
+#include <Arduino_FreeRTOS.h>
 #include <task.h>
 #include <semphr.h>
-//1 - definicja stałych dotyczących czasów 
-//2 - deklaracje tasków 
-//3 - utwórz uchwyty do tasków 
 
-const int ledPIN = 13; 
+const int ledPIN = 13;
+const int ledPIN_2 = 11;
 #define TASK_TIME_1 1000
-#define TASK_TIME_2 1000
+#define TASK_TIME_2 1100
 
-
-
-void Task1_Function(void *parametr); 
-void Task2_function(void *parametr); 
-SemaphoreHandle_t Led_semaphor;  // Semafor dla diody led 
+void Task1_Function(void *parametr);
+void Task2_function(void *parametr);
+SemaphoreHandle_t Led_semaphor;
 
 TaskHandle_t Task1;
-TaskHandle_t Task2; 
+TaskHandle_t Task2;
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPIN, OUTPUT);
+  pinMode(ledPIN_2, OUTPUT);
+  Led_semaphor = xSemaphoreCreateBinary();
+  xSemaphoreGive(Led_semaphor);
 
-void setup ()
-{
-  Serial.begin(9600); 
-pinMode(ledPIN,OUTPUT) ; // pin diody ustawiony jako wyjście 
-Led_semaphor = xSemaphoreCreateBinary(); 
+  xTaskCreate(Task1_Function, "TASK nr 1", 128, NULL, 1, &Task1);
+  xTaskCreate(Task2_function, "TASK nr 2", 128, NULL, 2, &Task2);
 
-xSemaphoreGive(Led_semphor); 
-
-
-  xTaskCreate(Task1_Function," TASK nr 1",128,NULL,2,&Task1) ;
-  xTaskCreate(Task2_function," TASK nr 2",128,NULL,1,&Task2) ;
-
- 
+  vTaskStartScheduler();
 }
 
+void loop() {
+  // Pusta pętla w loop, ponieważ obsługa zadań jest realizowana przez FreeRTOS
+}
 
-void Task1_Function(void *pvParametrs)
-{
-  while (1) 
-  {
-    xSemaphoreTake(Led_semaphor,500) ; 
+void Task1_Function(void *pvParametrs) {
+  while (1) {
+    xSemaphoreTake(Led_semaphor, portMAX_DELAY);
 
-    digitalWrite(ledPIN,1) ; 
-    vTaskDelay(pdMS_TO_TICKS(1000)); 
-    
-    digitalWrite(ledPIN,0) ; 
+    digitalWrite(ledPIN, 1);
+    digitalWrite(ledPIN_2, 0);
+    vTaskDelay(TASK_TIME_1);
 
-    xSemaphoreGive(Led_semaphor,1000); 
-
-    vTaskDelay(1000) ; 
+    digitalWrite(ledPIN, 0);
+    xSemaphoreGive(Led_semaphor);
+    vTaskDelay(TASK_TIME_1);
   }
 }
 
+void Task2_function(void *pvParametrs) {
+  while (1) {
+    xSemaphoreTake(Led_semaphor, portMAX_DELAY);
 
-void Task2(void *Parametrs)
-{
-  while{
-  xSemaphoreTake(Led_semaphor,300) ; 
+    digitalWrite(ledPIN, 0);
+    digitalWrite(ledPIN_2, 1);
+    vTaskDelay(TASK_TIME_2);
 
-  digitalWrite(ledPIN,1) ; 
-  vTaskDelay(200) ;
-
-  digitalWrite(ledPIN,0) ; 
-  vTaskDelay(300); 
-
-  xSemaphoreGive(Led_semaphor, portMAX_DELAY) ; 
+    digitalWrite(ledPIN_2, 0);
+    xSemaphoreGive(Led_semaphor);
+    vTaskDelay(TASK_TIME_2);
   }
 }
-
-
-//int main ()
-//{
-//  xTaskCreate(Task1_Function," TASK nr 1",128,NULL,2,&Task1) ;
-//  xTaskCreate(Task2_function," TASK nr 2",128,NULL,1,&Task2) ;
-//  vTaskStartScheduler(); 
-//  return 0 ; 
-//}
