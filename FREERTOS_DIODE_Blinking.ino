ee51f5e46d8771b356c4cbf36ff9e7689ddf89c0
RadoslawMusial <onlynaves@gmail.com>
2023-08-12 02:07:23 +0200

    one thread, but it working correct

--- FREERTOS_DIODE_Blinking.ino
+++ FREERTOS_DIODE_Blinking.ino
@@ -1,60 +1,112 @@
 #include <Arduino_FreeRTOS.h>
 #include <task.h>
 #include <semphr.h>
-
-const int ledPIN = 13;
-const int ledPIN_2 = 11;
+SemaphoreHandle_t Led_mutex; // Deklaracja mutexa
+const int ledPIN_RED_TRAFFIC = 12;
+const int ledPIN_YELLOW_TRAFFIC = 11;
+const int ledPIN_GREEN_TRAFFIC = 10;
+const int ledPIN_RED_PEDESTRIAN = 9;
+const int ledPIN_GREEN_PEDESTRIAN = 8;
 #define TASK_TIME_1 1000
 #define TASK_TIME_2 1100
+SemaphoreHandle_t Led_semaphor;
+
 
 void Task1_Function(void *parametr);
 void Task2_function(void *parametr);
-SemaphoreHandle_t Led_semaphor;
+void Task3_function(void *parametr);  // Dodane wywołanie Task3_function
+
 
 TaskHandle_t Task1;
 TaskHandle_t Task2;
+TaskHandle_t Task3;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(ledPIN, OUTPUT);
-  pinMode(ledPIN_2, OUTPUT);
+  pinMode(ledPIN_RED_TRAFFIC, OUTPUT);
+  pinMode(ledPIN_YELLOW_TRAFFIC, OUTPUT);
+  pinMode(ledPIN_GREEN_TRAFFIC, OUTPUT);
+  pinMode(ledPIN_RED_PEDESTRIAN, OUTPUT);
+  pinMode(ledPIN_GREEN_PEDESTRIAN, OUTPUT);
   Led_semaphor = xSemaphoreCreateBinary();
   xSemaphoreGive(Led_semaphor);
 
   xTaskCreate(Task1_Function, "TASK nr 1", 128, NULL, 1, &Task1);
-  xTaskCreate(Task2_function, "TASK nr 2", 128, NULL, 2, &Task2);
+//  xTaskCreate(Task2_function, "TASK nr 2", 128, NULL, 2, &Task2);
 
   vTaskStartScheduler();
 }
 
 void loop() {
-  // Pusta pętla w loop, ponieważ obsługa zadań jest realizowana przez FreeRTOS
+  
 }
 
 void Task1_Function(void *pvParametrs) {
   while (1) {
-    xSemaphoreTake(Led_semaphor, portMAX_DELAY);
-
-    digitalWrite(ledPIN, 1);
-    digitalWrite(ledPIN_2, 0);
-    vTaskDelay(TASK_TIME_1);
-
-    digitalWrite(ledPIN, 0);
-    xSemaphoreGive(Led_semaphor);
-    vTaskDelay(TASK_TIME_1);
+   
+      vTaskDelay(100) ; 
+      digitalWrite(ledPIN_GREEN_PEDESTRIAN, 0);
+      digitalWrite(ledPIN_RED_TRAFFIC, 1);
+      vTaskDelay(100) ; 
+      digitalWrite(ledPIN_RED_TRAFFIC, 0); 
+      digitalWrite(ledPIN_YELLOW_TRAFFIC, 1);
+      digitalWrite(ledPIN_RED_PEDESTRIAN, 1);
+      vTaskDelay(100) ; 
+       digitalWrite(ledPIN_YELLOW_TRAFFIC, 0);
+       digitalWrite(ledPIN_GREEN_TRAFFIC, 1);
+       
+       vTaskDelay(100) ; 
+       digitalWrite(ledPIN_RED_PEDESTRIAN, 1);
+            vTaskDelay(1000) ; 
+         digitalWrite(ledPIN_RED_PEDESTRIAN, 0);
+          vTaskDelay(100) ;
+         digitalWrite(ledPIN_GREEN_PEDESTRIAN, 0);
+         digitalWrite(ledPIN_GREEN_TRAFFIC, 0);
+      digitalWrite(ledPIN_YELLOW_TRAFFIC, 1);
+      vTaskDelay(100); 
+      digitalWrite(ledPIN_YELLOW_TRAFFIC, 0);
+      
+    digitalWrite(ledPIN_RED_TRAFFIC, 1);
+    digitalWrite(ledPIN_GREEN_PEDESTRIAN, 1);
+    vTaskDelay(1000) ; 
+          xSemaphoreGive(Led_semaphor);
   }
 }
 
-void Task2_function(void *pvParametrs) {
-  while (1) {
-    xSemaphoreTake(Led_semaphor, portMAX_DELAY);
+//void Task2_function(void *pvParametrs) {
+//  while (1) {
+//    
+//   
+//
+//   
+//    digitalWrite(ledPIN_GREEN_TRAFFIC, 0);
+//    digitalWrite(ledPIN_YELLOW_TRAFFIC, 1);
+//    vTaskDelay(100); 
+//    digitalWrite(ledPIN_YELLOW_TRAFFIC, 0);
+//    vTaskDelay(100);
+//  digitalWrite(ledPIN_RED_TRAFFIC, 1);
+//  digitalWrite(ledPIN_GREEN_PEDESTRIAN, 1);
+// 
+//  taskYIELD();
+//    vTaskDelay(100); 
+//  
+//
+//    vTaskDelay(3000); 
+//  }
+//}
 
-    digitalWrite(ledPIN, 0);
-    digitalWrite(ledPIN_2, 1);
-    vTaskDelay(TASK_TIME_2);
-
-    digitalWrite(ledPIN_2, 0);
-    xSemaphoreGive(Led_semaphor);
-    vTaskDelay(TASK_TIME_2);
-  }
-}
+//void Task3_function(void *pvParametrs) {
+//  while (1) {
+//    xSemaphoreTake(Led_semaphor, portMAX_DELAY);
+//
+//    digitalWrite(ledPIN_RED_PEDESTRIAN, 1);
+//    vTaskDelay(3000); 
+//    digitalWrite(ledPIN_RED_PEDESTRIAN, 0);
+//    digitalWrite(ledPIN_GREEN_PEDESTRIAN, 1);
+//    vTaskDelay(2000);
+//
+//    xSemaphoreGive(Led_semaphor);
+//
+//    vTaskDelay(100); 
+//  }
+//}
