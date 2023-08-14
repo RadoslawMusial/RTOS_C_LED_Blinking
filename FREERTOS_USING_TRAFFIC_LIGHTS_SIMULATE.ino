68f6d125c7cfbbf54d96843752539610ffa3ebc5
RadoslawMusial <107532472+RadoslawMusial@users.noreply.github.com>
2023-08-14 02:05:42 +0200

    added 2 new threads

--- FREERTOS_USING_TRAFFIC_LIGHTS_SIMULATE.ino
+++ FREERTOS_USING_TRAFFIC_LIGHTS_SIMULATE.ino
@@ -7,32 +7,48 @@ const int ledPIN_YELLOW_TRAFFIC = 11;
 const int ledPIN_GREEN_TRAFFIC = 10;
 const int ledPIN_RED_PEDESTRIAN = 9;
 const int ledPIN_GREEN_PEDESTRIAN = 8;
+const int ledPIN_RED_TRAFFIC_2 = 4;
+const int ledPIN_YELLOW_TRAFFIC_2 = 3;
+const int ledPIN_GREEN_TRAFFIC_2 = 2;
+const int ledPIN_GREEN_PEDESTRIAN_2 = A0;
+const int ledPIN_RED_PEDESTRIAN_2 = A1;
 #define TASK_TIME_1 1000
 #define TASK_TIME_2 1100
 SemaphoreHandle_t Led_semaphor;
+SemaphoreHandle_t Led_semaphor_2;
 
 
 void Task1_Function(void *parametr);
 void Task2_function(void *parametr);
 void Task3_function(void *parametr);  // Dodane wywołanie Task3_function
-
+void Task4_Function(void *parametr);
 
 TaskHandle_t Task1;
 TaskHandle_t Task2;
 TaskHandle_t Task3;
+TaskHandle_t Task4;
 
 void setup() {
   Serial.begin(9600);
+  
   pinMode(ledPIN_RED_TRAFFIC, OUTPUT);
   pinMode(ledPIN_YELLOW_TRAFFIC, OUTPUT);
   pinMode(ledPIN_GREEN_TRAFFIC, OUTPUT);
   pinMode(ledPIN_RED_PEDESTRIAN, OUTPUT);
   pinMode(ledPIN_GREEN_PEDESTRIAN, OUTPUT);
+  pinMode(ledPIN_GREEN_TRAFFIC_2, OUTPUT);
+  pinMode(ledPIN_YELLOW_TRAFFIC_2, OUTPUT);
+  pinMode(ledPIN_RED_TRAFFIC_2, OUTPUT);
+  pinMode(ledPIN_GREEN_PEDESTRIAN_2, OUTPUT);
+   pinMode(ledPIN_RED_PEDESTRIAN_2, OUTPUT);
   Led_semaphor = xSemaphoreCreateBinary();
+   Led_semaphor_2 = xSemaphoreCreateBinary();
   xSemaphoreGive(Led_semaphor);
 
   xTaskCreate(Task1_Function, "TASK nr 1", 128, NULL, 1, &Task1);
   xTaskCreate(Task2_function, "TASK nr 2", 128, NULL, 2, &Task2);
+  xTaskCreate(Task3_function, "TASK nr 3", 128, NULL, 1, &Task3);
+  xTaskCreate(Task4_function, "TASK nr 4", 128, NULL, 2, &Task4);
 
   vTaskStartScheduler();
 }
@@ -47,6 +63,8 @@ void Task1_Function(void *pvParametrs) {
 
     digitalWrite(ledPIN_GREEN_PEDESTRIAN, 0);
     digitalWrite(ledPIN_RED_TRAFFIC, 1);
+    
+   
     vTaskDelay(100) ;
     digitalWrite(ledPIN_RED_TRAFFIC, 0);
     digitalWrite(ledPIN_YELLOW_TRAFFIC, 1);
@@ -68,7 +86,7 @@ void Task1_Function(void *pvParametrs) {
 void Task2_function(void *param)
 {
   while (1) {
-
+  
     xSemaphoreTake(Led_semaphor, portMAX_DELAY) ;
     digitalWrite(ledPIN_RED_PEDESTRIAN, 1);
     vTaskDelay(100) ;
@@ -82,7 +100,53 @@ void Task2_function(void *param)
     digitalWrite(ledPIN_RED_PEDESTRIAN, 0);
     digitalWrite(ledPIN_GREEN_PEDESTRIAN, 1);
     vTaskDelay(1200) ;
-    xSemaphoreGive(Led_semaphor) ;
+    xSemaphoreGive(Led_semaphor) ; 
     vTaskDelay(1000) ;
   }
+}
+
+void Task3_function(void *param)
+{
+  while (1)
+  {
+    xSemaphoreTake(Led_semaphor_2,1000); 
+    digitalWrite(ledPIN_RED_PEDESTRIAN_2, 1);
+    vTaskDelay(100) ;
+    digitalWrite(ledPIN_GREEN_PEDESTRIAN_2, 0);
+    digitalWrite(ledPIN_GREEN_TRAFFIC_2, 0);
+    digitalWrite(ledPIN_YELLOW_TRAFFIC_2, 1);
+    vTaskDelay(100);
+    digitalWrite(ledPIN_YELLOW_TRAFFIC_2, 0);
+
+    digitalWrite(ledPIN_RED_TRAFFIC_2, 1);
+    digitalWrite(ledPIN_RED_PEDESTRIAN_2, 0);
+    digitalWrite(ledPIN_GREEN_PEDESTRIAN_2, 1);
+    vTaskDelay(1200) ;
+      xSemaphoreGive(Led_semaphor_2); 
+      vTaskDelay(1000); 
+  }
+}
+
+void Task4_function(void*param)
+{
+  while (1)
+///
+ {
+     
+  xSemaphoreTake(Led_semaphor_2,1000); 
+   digitalWrite(ledPIN_RED_PEDESTRIAN_2, 1);
+    vTaskDelay(100) ;
+    digitalWrite(ledPIN_GREEN_PEDESTRIAN_2, 0);
+    digitalWrite(ledPIN_GREEN_TRAFFIC_2, 0);
+    digitalWrite(ledPIN_YELLOW_TRAFFIC_2, 1);
+    vTaskDelay(100);
+    digitalWrite(ledPIN_YELLOW_TRAFFIC_2, 0);
+
+    digitalWrite(ledPIN_RED_TRAFFIC_2, 1);
+    digitalWrite(ledPIN_RED_PEDESTRIAN_2, 0);
+    digitalWrite(ledPIN_GREEN_PEDESTRIAN_2, 1);
+    vTaskDelay(1200) ;
+         xSemaphoreGive(Led_semaphor_2); 
+         vTaskDelay(1000); 
+ }
 }
